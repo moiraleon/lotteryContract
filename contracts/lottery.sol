adbaa710a017cd4c2f2f6f958e8f047da8ba31e4
Moira Leon <moira.leon2@gmail.com>
2022-03-05 12:02:28 -0700

    making sure players who enter the contest enter at a minimum .01 ether

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -9,9 +9,12 @@ contract Lottery{
     }
 
     function enter() public payable {
+        require(msg.value > .01 ether);
+
         players.push(msg.sender);
     }
 }
 
 
  //manager = msg.sender; //ensures that when we create this contract we get the senders address assigned to it
+ //msg.value is in wei so we need to convert our expectation to the expected eth in wei quantity
