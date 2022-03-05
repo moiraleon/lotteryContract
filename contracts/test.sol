4d4d4997467db1e4e42d2ea9c6b24eae95b7b668
Moira Leon <moira.leon2@gmail.com>
2022-03-04 17:42:26 -0700

    contract testing dynamic vs fixed arrays in solidity

---
+++ contracts/test.sol
@@ -0,0 +1,19 @@
+pragme solidity ^0.4.17;
+
+contract Test{
+    uint[] public myArray;
+
+    function Test() public{
+        myArray.push(1);
+        myArray.push(10);
+        myArrray.push(30);
+    }
+
+    function getArrayLength() public view returns (uint){
+        return myArray.length;
+    }
+
+    function getFirstElement() public view returns (uint){
+        return myArray[]0;
+    }
+}
