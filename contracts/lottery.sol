7334e9146aaae598502b718e74a023ce86f8acf3
Moira Leon <moira.leon2@gmail.com>
2022-03-04 17:31:41 -0700

    adding the senders address to the cntract

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -2,4 +2,11 @@ pragma solidity ^0.4.17;
 
 contract Lottery{
     address public manager;
+
+    function Lottery() public {
+        manager = msg.sender; 
+    }
 }
+
+
+ //manager = msg.sender; //ensures that when we create this contract we get the senders address assigned to it
