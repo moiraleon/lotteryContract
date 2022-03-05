7b57ec69bec24ea9131292be3de5d39d89877b7e
Moira Leon <moira.leon2@gmail.com>
2022-03-05 13:03:18 -0700

    using random() to pick the index of the winner

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.17;
 
-contract Lottery{
+contract Lottery {
     address public manager;
     address[] public players;
 
     function Lottery() public {
-        manager = msg.sender; 
+        manager = msg.sender;
     }
 
     function enter() public payable {
@@ -14,13 +14,16 @@ contract Lottery{
         players.push(msg.sender);
     }
 
-    function random() private view returns(uint) {
-        return uint(keccak256(block.difficulty, now, players));
+    function random() private view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, players));
+    }
+
+    function pickWinner() public {
+        uint256 index = random() % players.length;
     }
 }
 
-
- //manager = msg.sender; //ensures that when we create this contract we get the senders address assigned to it
- //msg.value is in wei so we need to convert our expectation to the expected eth in wei quantity
- //sha3 is global and is able to be called without imports, also may see keccak256() - keccak is a class of algorithms while sha3 is an instance of it
- //random funcitont akes the hash of each of those variable and converts it to a number - not perfectly random - can be manipulated - change random function to public for testing
+//manager = msg.sender; //ensures that when we create this contract we get the senders address assigned to it
+//msg.value is in wei so we need to convert our expectation to the expected eth in wei quantity
+//sha3 is global and is able to be called without imports, also may see keccak256() - keccak is a class of algorithms while sha3 is an instance of it
+//random funcitont akes the hash of each of those variable and converts it to a number - not perfectly random - can be manipulated - change random function to public for testing
